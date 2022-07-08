69aea2fd9ffb81fd4dcb7e5498ddd87b41fc0095
leonardo <amarantee.reis@gmail.com>
2022-07-07 21:22:29 -0300

    m

--- db/connection.js
+++ db/connection.js
@@ -3,13 +3,11 @@ require('dotenv').config();
 
 
 const con = mysql.createConnection({
-
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database : 'portloginsystem'
 });
-console.log(process.env.MYSQL_HOST)
 module.exports = con
 
